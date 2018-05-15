44adf4e94c6a9b3d22515c4c3cacbe7fd674f8cc
Ben Truong <bhtruon1@uci.edu>
2018-05-15 14:32:58 -0700

    setup basic database for Inverted Index

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-import mongoengine
+#!/usr/bin/python
+
+from pymongo import *
 
 #  Verify mongo exists and is running, set up database schema 
 
-mongoengine.connect('tokenInfo', host='localhost', port=27017)
+client = MongoClient('localhost', 27017)
+db = client['InvertedIndex']
+Dictionary = db["Dictionary"]
+
+# tokenInfo = {"token": "Ben", "Postings": {"doc1": 3.2, "doc2": 4}} 
+# token_id = Dictionary.insert(tokenInfo) 
+# print(Dictionary.find_one())
 
