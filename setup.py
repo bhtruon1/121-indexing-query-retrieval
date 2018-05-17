236ccad25219134ae3e88c543c95a890566538f4
mmarano25 <mmarano@uci.edu>
2018-05-16 18:22:34 -0700

    Added index to mongodb database to speed queries

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 client = MongoClient('localhost', 27017)
 db = client['InvertedIndex']
 Dictionary = db["Dictionary"]
+Dictionary.create_index([("token", ASCENDING)])
 
 # tokenInfo = {"token": "Ben", "Postings": {"doc1": 3.2, "doc2": 4}} 
 # token_id = Dictionary.insert(tokenInfo) 
